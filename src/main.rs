74eb560350798174e175faf1763bc6dca8c72407
pyfinart <mypyfinart@gmail.com>
2024-12-25 03:44:59 +0800

    feat:web server:new:增加tracing追踪异步请求的生命周期

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,18 @@ mod models;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
+    // Connect to the database
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
-
     let db = MySqlPool::connect(&database_url).await?;
+
+    // tracing
+    tracing_subscriber::fmt::init();
+
+    // Create the app state
     let app_state = AppState::new(db);
     let app = routes::create_app_router().with_state(app_state);
 
+    // Run the server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8900));
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
     axum::serve(listener, app)
--- src/routes.rs
+++ src/routes.rs
@@ -1,11 +1,16 @@
-use axum::Router;
+use axum::{http, Router};
 use axum::routing::{get, post};
 use crate::app_state::AppState;
 use crate::handlers::user_handler::{create_user_handler, user_login_handler};
+use tower_http::trace::{DefaultOnResponse, TraceLayer};
+use tracing::Level;
 
 pub fn create_app_router() -> Router<AppState> {
     Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/user/create", post(create_user_handler))
         .route("/user/login", post(user_login_handler))
+        .layer(TraceLayer::new_for_http()
+            .on_response(DefaultOnResponse::new().level(Level::INFO))
+        )
 }
