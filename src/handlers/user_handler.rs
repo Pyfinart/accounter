8b9213997288d826d9d06de0d711d837c4d764a2
pyfinart <mypyfinart@gmail.com>
2025-01-06 23:22:36 +0800

    feat:main:new:accounter服务端，统一接口返回协议

--- src/handlers/user_handler.rs
+++ src/handlers/user_handler.rs
@@ -2,23 +2,20 @@ use crate::app_state::AppState;
 use crate::errors::errs::AppError;
 use crate::models::resp_req::{ApiResponse, CreateUserRequest, CreateUserResponse, EmptyResponse, LoginRequest};
 use axum::extract::State;
-use axum::http::StatusCode;
 use axum::Json;
 
 #[axum_macros::debug_handler]
 pub async fn create_user_handler(
     State(state): State<AppState>,
     Json(req): Json<CreateUserRequest>,
-) -> (StatusCode, Json<CreateUserResponse>) {
+) -> Result<Json<ApiResponse<CreateUserResponse>>, AppError> {
     let user_service = &state.user_service.lock().await;
     let result = user_service.create_user(req.username, req.password).await;
     match result {
         Ok(id) => {
-            (StatusCode::CREATED, Json(CreateUserResponse { data: id, message: "success".to_string() }))
+            Ok(Json(ApiResponse::success(CreateUserResponse { data: id, message: "success".to_string() })))
         }
-        Err(msg) => {
-            (StatusCode::INTERNAL_SERVER_ERROR, Json(CreateUserResponse { data: 0, message: msg }))
-        }
+        Err(msg) => Err(AppError::BadRequest(msg))
     }
 }
 
